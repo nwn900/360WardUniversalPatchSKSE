fd18b8590981986d7299247d116e0d940f048010
nwn900 <nwn900@gmail.com>
2026-09-11 18:08:11 +0000

    Test custom ward art preservation

--- tests/ward_policy_tests.cpp
+++ tests/ward_policy_tests.cpp
@@ -6,14 +6,22 @@ int main()
 {
 	using WardVisualForwarder::EffectVisualSummary;
 	using WardVisualForwarder::IsWardPulseEvent;
-	using WardVisualForwarder::ShouldPlayNativePulse;
 	using WardVisualForwarder::ShouldForward;
+	using WardVisualForwarder::ShouldPlayNativePulse;
+	using WardVisualForwarder::ShouldReplaceArt;
 
 	assert(ShouldForward(EffectVisualSummary{ .wardPower = true }));
 	assert(ShouldForward(EffectVisualSummary{ .hitReferencesWard = true }));
 	assert(ShouldForward(EffectVisualSummary{ .castingReferencesWard = true }));
 	assert(ShouldForward(EffectVisualSummary{ .enchantReferencesWard = true }));
 	assert(!ShouldForward(EffectVisualSummary{}));
+
+	assert(ShouldReplaceArt(true, false, false));
+	assert(ShouldReplaceArt(true, true, true));
+	assert(!ShouldReplaceArt(true, true, false));
+	assert(ShouldReplaceArt(false, true, true));
+	assert(!ShouldReplaceArt(false, false, false));
+
 	assert(IsWardPulseEvent("OnWardHit"));
 	assert(IsWardPulseEvent("OnHit"));
 	assert(!IsWardPulseEvent("OnEffectStart"));
